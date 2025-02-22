2dc9e1f3b9537fa87384692432618b900d7e04d3
N03l-MG <noelmonzongarcia@gmail.com>
2025-02-22 17:59:24 +0100

    cpp01 ex04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+	std::string filename;
+	std::string s1;
+	std::string s2;
+
+	// Check and get arguments
+	if (argc != 4) {
+		std::cerr << "Error: invalid arguments.\n" <<
+		"./ex04 filename target_string replacement_string" << std::endl;
+		return 1;
+	} else {
+		filename = argv[1];
+		s1 = argv[2];
+		s2 = argv[3];
+	}
+
+	// Open input file
+	std::ifstream inputFile(filename);
+	if (!inputFile) {
+		std::cerr << "Error: could not open file." << std::endl;
+		return 1;
+	}
+
+	// Create output file
+	std::ofstream outputFile(filename + ".replace");
+	if (!outputFile) {
+		std::cerr << "Error: could not create file." << std::endl;
+		return 1;
+	}
+
+	// Cycle though input file and replace s1 with s2 using append and positions of s1
+	std::string line;
+	while (std::getline(inputFile, line))
+	{
+		std::string newLine;
+		size_t pos = 0;
+		while (pos < line.size())
+		{
+			size_t occurence = line.find(s1, pos);
+			if (occurence != std::string::npos) {
+				newLine.append(line, pos, occurence - pos);
+				newLine.append(s2);
+				pos = occurence + s1.size();
+			} else {
+				newLine.append(line, pos, line.size() - pos);
+				break;
+			}
+		}
+		outputFile << newLine << std::endl;
+	}
+
+	// Close files
+	inputFile.close();
+	outputFile.close();
+	return 0;
+}
