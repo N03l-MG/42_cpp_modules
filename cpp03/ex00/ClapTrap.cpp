8c3e15fe8b40ed1d5b46e8fdbf2b577c297dc65e
Noel Monzon <nmonzon@2-D-2.42heilbronn.de>
2025-03-04 18:33:17 +0100

    cpp03 ex00 almost

---
+++ cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmonzon <nmonzon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 16:53:47 by nmonzon           #+#    #+#             */
+/*   Updated: 2025/03/04 18:31:01 by nmonzon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+// Orthodox Canonical Form Defaults
+
+ClapTrap::ClapTrap(std::string name) : name(name), hitPoints(10), energyPoints(10), attackDamage(0)
+{
+	std::cout << "ClapTrap instance " << name << " constructed." << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &src)
+{
+	*this = src;
+	std::cout << "ClapTrap instance " << name << " copied from " << src.name
+	<< " and constructed." << std::endl;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &src)
+{
+	// std::cout << "Class copy assignment overload called." << std::endl;
+	name = src.name;
+	hitPoints = src.hitPoints;
+	energyPoints = src.energyPoints;
+	attackDamage = src.attackDamage;
+	return *this;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap instance " << name << " destroyed." << std::endl;
+}
+
+// Getters & Setters
+
+std::string ClapTrap::getName() const { return name; }
+
+unsigned int ClapTrap::getHitPoints() const { return hitPoints; }
+
+unsigned int ClapTrap::getEnergyPoints() const { return energyPoints; }
+
+unsigned int ClapTrap::getAttackDamage() const { return attackDamage; }
+
+void ClapTrap::setName(std::string name) { this->name = name; }
+
+void ClapTrap::setHitPoints(unsigned int hitPoints) { this->hitPoints = hitPoints; }
+
+void ClapTrap::setEnergyPoints(unsigned int energyPoints) { this->energyPoints = energyPoints; }
+
+void ClapTrap::setAttackDamage(unsigned int attackDamage) { this->attackDamage = attackDamage; }
+
+// Class Methods (will change, these suck.)
+
+void ClapTrap::attack(std::string const &target)
+{
+	if (energyPoints > 0 && hitPoints > 0) {
+		energyPoints--;
+		std::cout << "ClapTrap " << name << " attacks " << target
+		<< ", causing " << attackDamage << " points of damage!" << std::endl;
+	} else {
+		std::cout << "ClapTrap " << name << " is out of energy or HP."
+		<< std::endl;
+	}
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (amount > 0) {
+		if (hitPoints > amount) {
+			hitPoints -=amount;
+			std::cout << "ClapTrap " << name << " takes " << amount
+					  << " points of damage!" << std::endl;
+		} else {
+			hitPoints = 0;
+			std::cout << "ClapTrap " << name << " takes " << amount
+					  << " points of damage... It's HP has reached 0!" << std::endl;
+		}
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (energyPoints > 0 && hitPoints > 0) {
+		energyPoints--;
+		hitPoints += amount;
+		std::cout << "ClapTrap " << name << " repairs itself for " << amount
+				  << " of hit points and is now at " << hitPoints<< " HP." << std::endl;
+	} else
+		std::cout << "ClapTrap " << name << " has insufficient energy or HP." << std::endl;
+}
---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmonzon <nmonzon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 16:48:35 by nmonzon           #+#    #+#             */
+/*   Updated: 2025/03/04 18:12:00 by nmonzon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	std::string name;
+	int hitPoints;
+	int energyPoints;
+	int attackDamage;
+public:
+	// OCF Defaults
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &src);
+	ClapTrap &operator=(const ClapTrap &src);
+	~ClapTrap();
+
+	// Getters & Setters
+	std::string getName() const;
+	unsigned int getHitPoints() const;
+	unsigned int getEnergyPoints() const;
+	unsigned int getAttackDamage() const;
+	void setName(std::string name);
+	void setHitPoints(unsigned int hitPoints);
+	void setEnergyPoints(unsigned int energyPoints);
+	void setAttackDamage(unsigned int attackDamage);
+
+	// Class Methods
+	void attack(std::string const &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
+#endif
